d564702864586a77342975f350ea8774c4c3492f
vincenzoxx <50289647+vincenzoxx@users.noreply.github.com>
2019-05-05 12:25:33 +0200

    Commit from VS

--- FileExplorerTree/Models/StretchingTreeView.cs
+++ FileExplorerTree/Models/StretchingTreeView.cs
@@ -9,50 +9,50 @@ using System.Windows.Controls;
 namespace FileExplorerTree.Models
 {
     //I didn't use this because I've no clue yet how to fix it
-    public class StretchingTreeView : TreeView
-    {
+    //public class StretchingTreeView : TreeView
+    //{
 
-        protected override DependencyObject GetContainerForItemOverride()
-        {
-            return new StretchingTreeViewItem();
-        }
+    //    protected override DependencyObject GetContainerForItemOverride()
+    //    {
+    //        return new StretchingTreeViewItem();
+    //    }
 
-        protected override bool IsItemItsOwnContainerOverride(object item)
-        {
-            return item is StretchingTreeViewItem;
-        }
-    }
+    //    protected override bool IsItemItsOwnContainerOverride(object item)
+    //    {
+    //        return item is StretchingTreeViewItem;
+    //    }
+    //}
 
-    class StretchingTreeViewItem : TreeViewItem
-    {
-        public StretchingTreeViewItem()
-        {
-            this.Loaded += new RoutedEventHandler(StretchingTreeViewItem_Loaded);
-        }
+    //class StretchingTreeViewItem : TreeViewItem
+    //{
+    //    public StretchingTreeViewItem()
+    //    {
+    //        this.Loaded += new RoutedEventHandler(StretchingTreeViewItem_Loaded);
+    //    }
 
-        private void StretchingTreeViewItem_Loaded(object sender, RoutedEventArgs e)
-        {
-            // The purpose of this code is to stretch the Header Content all the way accross the TreeView. 
-            if (this.VisualChildrenCount > 0)
-            {
-                Grid grid = this.GetVisualChild(0) as Grid;
-                if (grid != null && grid.ColumnDefinitions.Count == 3)
-                {
-                    // Remove the middle column which is set to Auto and let it get replaced with the 
-                    // last column that is set to Star.
-                    grid.ColumnDefinitions.RemoveAt(1);
-                }
-            }
-        }
+    //    private void StretchingTreeViewItem_Loaded(object sender, RoutedEventArgs e)
+    //    {
+    //        // The purpose of this code is to stretch the Header Content all the way accross the TreeView. 
+    //        if (this.VisualChildrenCount > 0)
+    //        {
+    //            Grid grid = this.GetVisualChild(0) as Grid;
+    //            if (grid != null && grid.ColumnDefinitions.Count == 3)
+    //            {
+    //                // Remove the middle column which is set to Auto and let it get replaced with the 
+    //                // last column that is set to Star.
+    //                grid.ColumnDefinitions.RemoveAt(1);
+    //            }
+    //        }
+    //    }
 
-        protected override DependencyObject GetContainerForItemOverride()
-        {
-            return new StretchingTreeViewItem();
-        }
+    //    protected override DependencyObject GetContainerForItemOverride()
+    //    {
+    //        return new StretchingTreeViewItem();
+    //    }
 
-        protected override bool IsItemItsOwnContainerOverride(object item)
-        {
-            return item is StretchingTreeViewItem;
-        }
-    }
+    //    protected override bool IsItemItsOwnContainerOverride(object item)
+    //    {
+    //        return item is StretchingTreeViewItem;
+    //    }
+    //}
 }
