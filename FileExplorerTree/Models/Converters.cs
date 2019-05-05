a5f2b262f1664702c19e45bf70916021c51ef75e
vincenzoxx <50289647+vincenzoxx@users.noreply.github.com>
2019-05-05 17:09:41 +0200

    Expanded Visibility and Toggle

--- FileExplorerTree/Models/Converters.cs
+++ FileExplorerTree/Models/Converters.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace FileExplorerTree.Models
@@ -42,6 +43,35 @@ namespace FileExplorerTree.Models
                 return Math.Round((num / Math.Pow(1024f, powCounter-1))).ToString("N0") + " " + (FileSizeLabel)(powCounter);
             else
                 return (num / Math.Pow(1024f, powCounter-1)).ToString("N2") + " " + (FileSizeLabel)(powCounter+1);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class TypeOfNodeToVisibilityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            TypeOfNode t = TypeOfNode.Unknown;
+            try
+            {
+                t = (TypeOfNode)value;
+            }
+            catch
+            {
+
+            }
+            switch (t)
+            {
+                case TypeOfNode.Directory:
+                    return false;
+                 default:
+                    return true;
+            }
+            
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
